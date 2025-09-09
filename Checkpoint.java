052bdefdae5a180c6c790b3664e44180c3dcfd9a
Prasanna11-22 <psp444333@gmail.com>
2025-09-09 23:16:18 +0530

    Checkpoint.java

--- Checkpoint.java
+++ Checkpoint.java
@@ -2,17 +2,15 @@ package assignment;
 
 public class Checkpoint {
     private String id;
-    private String name; // Check-in, Security, Loading, etc.
+    private String name;
     private String timestamp;
 
-    // Constructor
     public Checkpoint(String id, String name, String timestamp) {
         this.id = id;
         this.name = name;
         this.timestamp = timestamp;
     }
 
-    // Getters
     public String getId() { return id; }
     public String getName() { return name; }
     public String getTimestamp() { return timestamp; }
@@ -22,4 +20,5 @@ public class Checkpoint {
         return "[" + timestamp + "] " + name + " (ID: " + id + ")";
     }
 }
+
 
